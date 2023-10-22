e4fe76a87e0491545b0490153c8b1ca33d8680ce
Maryam <maryammyasserr912@gmail.com>
2023-10-22 22:59:03 +0300

    add Splash Screen to the project

--- lib/pages/splash/splash_view.dart
+++ lib/pages/splash/splash_view.dart
@@ -1,11 +1,38 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashView extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:news_app/pages/home/home_view.dart';
+
+class SplashView extends StatefulWidget {
   static const String routeName = 'Splash';
   const SplashView({super.key});
 
   @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushNamedAndRemoveUntil(context, HomeView.routeName, (route) => false);
+    });
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        image: DecorationImage(
+            image: AssetImage('assets/images/pattern.png'),
+            fit: BoxFit.cover
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Center(child: Image.asset('assets/images/logo.png')),
+      ),
+    );
   }
 }
